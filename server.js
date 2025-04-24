c0e10f7cdd4afb1e7e84923a9162868005f4a45a
João Pedro Lima <joaopedrocal90@gmail.com>
2025-04-24 15:45:30 -0300

    feat: crio rotas para testes e instalo nodemon

--- server.js
+++ server.js
@@ -2,9 +2,15 @@ import http from "http";
 
 const PORT = 3000;
 
+const routes = {
+  "/": "Curso de node.js!",
+  "/livros": "Entrei na rota de livros!",
+  "/autores": "Entrei na rota de autores!",
+};
+
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "content-type": "text/plain" });
-  res.end("Curso de node.js!");
+  res.end(routes[req.url]);
 });
 
 server.listen(PORT, () => {
