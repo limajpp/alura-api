d38345233705108a9997ff727f7dc8053dff2ff5
João Pedro Lima <joaopedrocal90@gmail.com>
2025-04-25 02:00:59 -0300

    feat: altero lógica de acordo com o express framework

--- server.js
+++ server.js
@@ -1,18 +1,7 @@
-import http from "http";
+import app from "./src/app.js";
 
 const PORT = 3000;
 
-const routes = {
-  "/": "Curso de node.js!",
-  "/livros": "Entrei na rota de livros!",
-  "/autores": "Entrei na rota de autores!",
-};
-
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { "content-type": "text/plain" });
-  res.end(routes[req.url]);
-});
-
-server.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}...`);
 });
